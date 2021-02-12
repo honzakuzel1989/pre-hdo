93e5ada83bb4ed00a1ab87a06d9d9695c4a92849
Jan Kužel <jan.kuzel@alza.cz>
2021-02-12 22:03:58 +0100

    Add pre-hdo.Api project

---
+++ Src/pre-hdo.Api/Controllers/PreHdoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pre_hdo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PreHdoController : ControllerBase
+    {
+        private readonly ILogger<PreHdoController> _logger;
+
+        public PreHdoController(ILogger<PreHdoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new { a = 1, b = 2 };
+        }
+    }
+}
