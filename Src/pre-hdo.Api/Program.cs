9a8df36cd4459da5e36250f48440659be864d3f7
Jan Kužel <jan.kuzel@alza.cz>
2021-02-13 16:48:55 +0100

    UseUrls

--- Src/pre-hdo.Api/Program.cs
+++ Src/pre-hdo.Api/Program.cs
@@ -20,6 +20,7 @@ namespace pre_hdo.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
